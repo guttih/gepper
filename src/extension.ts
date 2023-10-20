9bbed868c69fcaf96465b62275f3117cde5ce998
Guðjón Hólm <gudjonholm@gmail.com>
2023-10-20 13:20:06 +0000

    Adoc to md (#47)
    * working on converting selected text from adoc to md
    
    * bumping to 0.6.9
    
    ---------
    
    Co-authored-by: gudjon <gudjon@tern.is>

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,3 @@
-
 import {
     workspace,
     ExtensionContext,
@@ -18,7 +17,6 @@ import { Projector } from "./Projector";
 let editorMenuHandler: MenuEditorHandler = new MenuEditorHandler();
 
 export function activate(context: ExtensionContext) {
-
     // window.showInformationMessage("Activating  gepper");
     let outputChannel: OutputChannel | null = null;
 
@@ -71,7 +69,6 @@ export function activate(context: ExtensionContext) {
     });
 
     let fnAddClassOperators = commands.registerCommand("gepper.addClassOperators", async (context) => {
-       
         const ret = await ClassWorker.addClassOperators();
     });
     let fnCreateCMakeProject = commands.registerCommand("gepper.createCMakeProject", async (context) => {
@@ -104,6 +101,132 @@ export function activate(context: ExtensionContext) {
         }
     });
 
+    function convertToAsciiDocHeadingsToMarkdown(text: string): string {
+        // Converting AsciiDoc headings to Markdown headings
+        return text.replace(/(=+)\s(.+?)\n/g, (match: string, level: string, content: string) => {
+            const markdownLevel = level.length;
+            return "#".repeat(markdownLevel) + " " + content + "\n";
+        });
+    }
+
+    
+    /**
+     *  Converts AsciiDoc User Story document ID links Git issue ID links
+     * 
+     * 
+     * @param content a AsciiDoc text
+     * @returns a Markdown text
+     */
+    function convertUserStoryIdsToGitIssueIds(content: string): string {
+        // Converting AsciiDoc headings to Markdown headings
+
+        //ID <<US051>> to be converted to #51
+        //remove the last >>
+        content =  content.replace(/(?<=US([0]{0,3}\d{1,}))>>/g, "");
+        //replacing  <<US051 with #51
+        content = content.replace(/<<US([0]{0,3}(?<=\d{1,}))/g, "#");
+
+        return content;
+
+    }
+
+    // Define a function to convert AsciiDoc-style lists with headings to Markdown lists
+    function convertToAsciiDocListToMarkdown(content: string): string {
+        // Replace heading and list item markers with Markdown list markers
+
+
+        content = content.replace(/\n\.\.\.\.\.\s/g, "\n                1. ");
+        content = content.replace(/\n\.\.\.\.\s/g,   "\n            1. ");
+        content = content.replace(/\n\.\.\.\s/g,     "\n        1. ");
+        content = content.replace(/\n\.\.\s/g,       "\n    1. ");
+        content = content.replace(/\n\.\s/g,       "\n1. ");
+
+        content = content.replace(/\n\*\*\*\*\*\s/g, "\n                * ");
+        content = content.replace(/\n\*\*\*\*\s/g,   "\n            * ");
+        content = content.replace(/\n\*\*\*\s/g,     "\n        * ");
+        content = content.replace(/\n\*\*\s/g,       "\n    * ");
+        content = content.replace(/\n\*\s/g,       "\n* ");
+
+        content = content.replace(/\n-----\s/g, "\n                * ");
+        content = content.replace(/\n----\s/g,   "\n            * ");
+        content = content.replace(/\n---\s/g,     "\n        * ");
+        content = content.replace(/\n--\s/g,       "\n    * ");
+        content = content.replace(/\n-\s/g,       "\n* ");
+
+        return content
+            .replace(/^ - (.+?) -\s*\n/gm, (match: string, heading: string) => {
+                return `**${heading}**\n`;
+            })
+            .replace(/^(\s*)\.\s+(.+?)\s*\n/gm, (match: string, leadingWhitespace: string, item: string) => {
+                // Check for leading whitespace to determine if it's a numbered list
+                if (leadingWhitespace) {
+                    return `${leadingWhitespace}1. ${item}\n`;
+                } else {
+                    return `${leadingWhitespace}- ${item}\n`;
+                }
+            });
+    }
+
+    /**
+     * Converts text to a snippet body
+     *
+     * @param {string} content the text to be converted
+     * @returns {string} Array of strings which can be used in a snippet body.
+     */
+    function asciiDocContentToMarkdown(content: string): string {
+        content = convertToAsciiDocListToMarkdown(content);
+        content = convertToAsciiDocHeadingsToMarkdown(content);
+        return content;
+    }
+
+    let message = commands.registerCommand("gepper.gepperMessage", () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        window.showInformationMessage("Yes, the gepper extension is running!");
+    });
+
+    
+
+    let asciiUserStoriesToGitIssue = commands.registerCommand("gepper.selTextUserStoryToIssues", async () => {
+        const editor = window.activeTextEditor;
+        if (editor) {
+            if (editor.selection.isEmpty) {
+                window.showErrorMessage("You need select a text first for convert to a snippet");
+            } else {
+                let txt = editor;
+                let content = txt.document.getText(txt.selection);
+
+                let output = convertUserStoryIdsToGitIssueIds(asciiDocContentToMarkdown(content));
+                if (outputChannel === null) {
+                    outputChannel = window.createOutputChannel("Markdown");
+                }
+                outputChannel.clear();
+                outputChannel.append(output);
+                outputChannel.show();
+            }
+        }
+    });
+
+    let asciiToMd = commands.registerCommand("gepper.selTextAsciiDocToMarkdown", async () => {
+        const editor = window.activeTextEditor;
+        if (editor) {
+            if (editor.selection.isEmpty) {
+                window.showErrorMessage("You need select a text first for convert to a snippet");
+            } else {
+                let txt = editor;
+                let content = txt.document.getText(txt.selection);
+
+                let output = asciiDocContentToMarkdown(content);
+                if (outputChannel === null) {
+                    outputChannel = window.createOutputChannel("Markdown");
+                }
+                outputChannel.clear();
+                outputChannel.append(output);
+                outputChannel.show();
+            }
+        }
+    });
+
     context.subscriptions.push(
         fnCreateClass,
         fnCreateClassInFolder,
@@ -114,7 +237,9 @@ export function activate(context: ExtensionContext) {
         fnOnDidChangeTextDocument,
         fnOnDidChangeTextEditorSelection,
         fnOnDidChangeActiveTextEditor,
-        fnRemoveDocumentComments
+        fnRemoveDocumentComments,
+        asciiToMd,
+        asciiUserStoriesToGitIssue,
     );
 
     //Run the menu check, at extension activation.
